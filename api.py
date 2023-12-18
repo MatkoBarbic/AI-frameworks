2155dec93509289812b94d652b968dfc226ed622
Matko <matko.barbic@deegloo.com>
2023-12-18 15:46:45 +0100

    Adding text predictions

--- api.py
+++ api.py
@@ -4,9 +4,11 @@ import torchvision.transforms as transforms
 from flask import Flask, jsonify, request
 from PIL import Image
 import io
-from model import model
+from posters_model import model as poster_model
+from bard import model as text_model
 from annoy import AnnoyIndex
 import pandas as pd
+from transformers import DistilBertTokenizerFast
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -14,15 +16,17 @@ app = Flask(__name__)
 dim = 576
 k = 5
 
-df = pd.read_csv("./movie_poster_paths.csv")
-df = df.apply(lambda x: "./" + x)
+posters_df = pd.read_csv("./movie_poster_paths.csv")
+posters_df = posters_df.apply(lambda x: "./" + x)
 transform = transforms.Compose([
     transforms.ToTensor(),
     # transforms.Normalize((0.5,), (0.5,))
 ])
 
-@app.route('/predict', methods=['POST'])
-def predict():
+text_df = pd.read_csv("./movies_metadata.csv")
+
+@app.route('/poster_predict', methods=['POST'])
+def poster_predict():
     img_binary = request.data
     img_pil = Image.open(io.BytesIO(img_binary))
 
@@ -32,24 +36,48 @@ def predict():
 
     # Get embeddings
     with torch.no_grad():
-        query_vector = model(tensor)[0]
+        query_vector = poster_model(tensor)[0]
 
     annoy_index = AnnoyIndex(dim, 'angular')
     annoy_index.load('./rec_imdb.ann')
 
     indices = annoy_index.get_nns_by_vector(query_vector, k)
 
-    recommended_paths = list(df.iloc[indices]["path"])
-
-    # recommendations_for_current_image = []
-    # for recommended_path in recommended_paths:
-    #     recommended_img_pil = Image.open(recommended_path)
-    #     recommended_img_data = io.BytesIO()
-    #     recommended_img_pil.save(recommended_img_data, format='JPEG')
-    #     recommended_img_data.seek(0)
-    #     recommendations_for_current_image.append(recommended_img_data.read())
+    recommended_paths = list(posters_df.iloc[indices]["path"])
 
     return jsonify({"recommendations": recommended_paths})
+
+
+@app.route('/description_predict', methods=['POST'])
+def description_predict():
+    description = request.data["description"]
+    radio = request.data["radio"]
+    
+    tensor = transform(description).to(device)
+    tensor = tensor.unsqueeze(0)  # Add batch dimension
+
+    if radio == "Bag of words":
+        with torch.no_grad():
+            query_vector = bow(tensor)[0]
+
+        annoy_index = AnnoyIndex(dim, 'angular')
+        annoy_index.load('./description_embeddings_bow.ann')
+    
+    else:
+        tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+        tensor = tokenizer(tensor)
+        with torch.no_grad():
+            query_vector = text_model(tensor)[0]
+
+        annoy_index = AnnoyIndex(dim, 'angular')
+        annoy_index.load('./description_embeddings_bard.ann')
+
+    indices = annoy_index.get_nns_by_vector(query_vector, k)
+
+    recommended_titles = list(text_df.iloc[indices]["title"])
+
+    return jsonify({"recommendations": recommended_titles})
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ bard.py
@@ -0,0 +1,30 @@
+from transformers import  DistilBertForSequenceClassification
+import torch.nn as nn
+
+class BertClf(nn.Module):
+
+    def __init__(self, distilbert):
+
+        super(BertClf, self).__init__()
+
+        self.distilbert = distilbert
+        for name, param in distilbert.named_parameters():
+            if not "classifier" in name:
+                param.requires_grad = False
+
+    def forward(self, sent_id, mask):
+
+        out = self.distilbert(sent_id, attention_mask=mask)
+        logits = out.logits
+        attn = out.attentions
+        hidden_states = out.hidden_states
+
+
+        return logits,hidden_states,attn
+
+distilbert = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased",
+                                                                  num_labels=4,
+                                                                  output_attentions=True,
+                                                                  output_hidden_states=True)
+
+model = BertClf(distilbert)
